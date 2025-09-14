fedadb90ce1b93522b01e23f49da1b67c624c848
AhutGupta <ahutgupta@gmail.com>
2025-09-13 17:57:10 -0700

    working poc

---
+++ appdata/jellyfin/data/data/ScheduledTasks/3a025083-141d-3c17-dd96-d5f9b951287b.js
@@ -0,0 +1,1 @@
+{"StartTimeUtc":"2025-09-14T00:51:20.9994958Z","EndTimeUtc":"2025-09-14T00:51:21.0374286Z","Status":"Completed","Name":"Clean up collections and playlists","Key":"CleanCollectionsAndPlaylists","Id":"3a025083141d3c17dd96d5f9b951287b"}
---
+++ appdata/jellyfin/data/data/ScheduledTasks/7d8088c1-0902-f1bf-4072-ded42437bcfb.js
@@ -0,0 +1,1 @@
+{"StartTimeUtc":"2025-09-14T00:51:20.9979637Z","EndTimeUtc":"2025-09-14T00:51:21.038904Z","Status":"Completed","Name":"Clean Transcode Directory","Key":"DeleteTranscodeFiles","Id":"7d8088c10902f1bf4072ded42437bcfb"}
